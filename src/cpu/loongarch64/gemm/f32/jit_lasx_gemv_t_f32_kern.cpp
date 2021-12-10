2a54b30327a15239b84addfa43d19b29feae949a
sunxin <sunxin-hf@loongson.cn>
2021-12-10 15:35:22 +0800

    load 9th param from sp not +8

--- src/cpu/loongarch64/gemm/f32/jit_lasx_gemv_t_f32_kern.cpp
+++ src/cpu/loongarch64/gemm/f32/jit_lasx_gemv_t_f32_kern.cpp
@@ -344,7 +344,7 @@ void jit_lasx_gemv_t_f32_kern::generate() {
 
     //mov(Y_, arg_y_); // Y_is abi_param8
     //mov(INCY_, arg_incy_);
-    ld_d(INCY_, sp, get_size_of_abi_save_regs() + 8); // INCY_ is 9 param in sp
+    ld_d(INCY_, sp, get_size_of_abi_save_regs()); // INCY_ is 9 param in sp
 
     //sub(A_, -offset_a_ * size_);
     add_imm(A_, A_, offset_a_ * size_, X_TMP_0);
